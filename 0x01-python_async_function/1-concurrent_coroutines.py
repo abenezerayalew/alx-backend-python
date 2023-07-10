b2639ec94b5f65772a5fda21a2f90a3e3e733f0b
abenezerayalew <aben.man.unity@gmail.com>
2023-07-11 00:10:29 +0300

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """ Async basics in Python task 1 """
-import random
+import asyncio
 from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int,max_delay: int) -> List[float]:
     """ Waits for a random delay between 0 and max_delay """
-    delay_list = []
-    for _ in range(n):
-        delay_list.append(await wait_random(max_delay))
-    return sorted(delay_list)
+    wait_times = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return sorted(wait_times)
