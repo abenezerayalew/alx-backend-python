204992851aadc0013a32f7cdfd5b5481620e32ed
abenezerayalew <aben.man.unity@gmail.com>
2023-07-12 00:02:51 +0300

    task1

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,6 +10,6 @@ async_comprehension = using("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """ Return time """
-    start = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.perf_counte
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    return time.time() - start_time
