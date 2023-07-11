ff762ddea2f10f62b95623a336443846a92d4bd6
abenezerayalew <aben.man.unity@gmail.com>
2023-07-11 23:52:17 +0300

    task0fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,14 @@
-#!/bin/user/env python3
-""" Async Generator """
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """ Async Generator """
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
